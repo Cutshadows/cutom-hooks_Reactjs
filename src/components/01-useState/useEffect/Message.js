ffd451fad4598b8eed50d4edf22ecf05a9c523a4
Douglas Barraza <dbarraza@acid.cl>
2021-03-06 23:51:35 -0300

    useEffect corte de funcionalidad para que no afecte

--- src/components/01-useState/useEffect/Message.js
+++ src/components/01-useState/useEffect/Message.js
@@ -1,19 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const Message = () => {
     
-    useEffect(() => {
-        console.log('componente montado')
-        return () => {
-            console.log('componente desmontado')
+    const [coords, setCoords] = useState({
+        x:0,
+        y:0
+    })
 
+    useEffect(() => {
+        const mouseMove=(e)=>{
+            const coords={x: e.x, y:e.y};
+            setCoords(coords) 
+         }
+        window.addEventListener('mousemove', mouseMove)
+
+        return () => {
+            window.removeEventListener('mousemove', mouseMove);
         }
-    }, [])
+    }, [coords])
+    const {x, y}=coords;
 
     return ( 
         <div> 
             <h3>Eres genial</h3>
+            <h3>X:{x} Y:{y}</h3>
         </div>
      );
 }
